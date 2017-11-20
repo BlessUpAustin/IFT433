249c7945edb8d373d4ed914145a88fba4750998e
vincewho <thisguyvince@gmail.com>
2017-11-20 12:26:02 -0700

    Update IFT433 Final Proj Select.sql
    Adding One more select

--- IFT433 Final Proj Select.sql
+++ IFT433 Final Proj Select.sql
@@ -9,6 +9,7 @@ Select * from Inventory order by ListPrice Desc;
 /*Sample recipt select statement with joins here. Using the joing statments we determine the 
 customer and the employee that put in the order. It is ordered by CustomerID.*/
 
+
 Select 
 	--*,
 	Sales.invoiceID,
@@ -17,6 +18,17 @@ Select
 	Sales.SaleTime,
 	Cus.FirstName + ' ' + Cus.LastName As CustomerName
 From
-Customer Cus Join Sales On Cus.CustID = Sales.CustID
+Customer Cus 
+	Join Sales On Cus.CustID = Sales.CustID
+	Join StoreSaleLineItem SS On Sales.invoiceID = SS.invoiceID
 Order By 
 	Cus.CustID;
+
+/*When I want to price on the invoice. I would use this code to get the invoice total*/
+Select 
+	SS.invoiceID,
+	Sum(SS.SalePice * SS.Quantity) As Sum 
+From 
+	Sales Join StoreSaleLineItem SS On Sales.invoiceID = SS.invoiceID
+Group By
+	SS.invoiceID;
