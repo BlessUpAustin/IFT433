9add9cf28115868e078ac66ae2f7bbd5c68b8ef9
vincewho <thisguyvince@gmail.com>
2017-11-20 14:18:58 -0700

    Update IFT433 Final Proj Select.sql
    Added one select

--- IFT433 Final Proj Select.sql
+++ IFT433 Final Proj Select.sql
@@ -10,25 +10,53 @@ Select * from Inventory order by ListPrice Desc;
 customer and the employee that put in the order. It is ordered by CustomerID.*/
 
 
+Use IFTProject;
+
+Select * from Customer;
+Select * from Employee;
+Select * from pay;
+Select * from StoreSaleLineItem order by invoiceID;
+Select * from Sales;
+Select * from Inventory order by ListPrice Desc;
+Select * from Supplier;
+
+/*Sample recipt select statement with joins here. Using the joing statments we determine the 
+customer and the employee that put in the order. It is ordered by CustomerID.*/
+
 Select 
-	--*,
-	Sales.invoiceID,
+	SS.invoiceID,
+	Sum(SS.SalePice * SS.Quantity) As InvoiceTotal,
 	Cus.CustID,
 	Sales.EmpID,
 	Sales.SaleTime,
-	Cus.FirstName + ' ' + Cus.LastName As CustomerName
+	cus.LastName+', '+ Cus.FirstName As CustomerName
 From
-Customer Cus 
+	Customer Cus 
 	Join Sales On Cus.CustID = Sales.CustID
 	Join StoreSaleLineItem SS On Sales.invoiceID = SS.invoiceID
-Order By 
-	Cus.CustID;
-
-/*When I want to price on the invoice. I would use this code to get the invoice total*/
-Select 
-	SS.invoiceID,
-	Sum(SS.SalePice * SS.Quantity) As Sum 
-From 
-	Sales Join StoreSaleLineItem SS On Sales.invoiceID = SS.invoiceID
 Group By
-	SS.invoiceID;
+	SS.invoiceID, Cus.CustID, Sales.EmpID, Sales.SaleTime, Cus.FirstName, Cus.LastName
+Order By 
+	SS.invoiceID
+;
+
+/*I want to get the top ten items in our inventory that is more than the average price of all items in the invetory.
+As well as the inventory so that if we need to order more we can */
+
+Select Top 10
+	InventoryID, 
+	itemName,
+	ListPrice,
+	Stock,
+	Case
+		When Stock < 30 Then 'Buy'
+		Else 'Enough stock'
+	End As 'Should We Order?'
+From
+	Inventory
+Where
+	ListPrice > (Select AVG(SalePice) From StoreSaleLineItem)
+Order By 
+	ListPrice Desc
+;
+
